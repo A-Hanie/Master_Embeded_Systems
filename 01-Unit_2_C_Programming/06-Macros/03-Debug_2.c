cfc4ab50ab52a3ee5f18d6f8bf16cd94db3bedc7
Ahmed Hanie <114179387+A-Hanie@users.noreply.github.com>
2022-12-04 10:08:41 +0200

    Create 03-Debug_2.c

---
+++ 01-Unit_2_C_Programming/06-Macros/03-Debug_2.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+#define DEBUG 1
+void debug_variable(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+/* debug_var(("var1 = %d", var1)); */
+#define debug_var(x)                                                 \
+    do                                                               \
+    {                                                                \
+        if (DEBUG)                                                   \
+        {                                                            \
+            debug_variable("@ File: %s, Func: %s(), Line: %d ---> ", \
+                           __FILE__, __func__, __LINE__);            \
+            debug_variable x;                                        \
+        }                                                            \
+    } while (0)
+
+int main()
+{
+    int var1 = 5;
+
+    debug_var(("var1 = %d , var1 = %d, var1 = %d, var1 = %d", var1++,var1++,++var1,var1));
+    return 0;
+}
