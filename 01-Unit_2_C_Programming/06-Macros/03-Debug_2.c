64982a148fc79a16bde9993b94af145886c614f4
Ahmed Hanie <114179387+A-Hanie@users.noreply.github.com>
2022-12-04 11:59:17 +0200

    Update 03-Debug_2.c

--- 01-Unit_2_C_Programming/06-Macros/03-Debug_2.c
+++ 01-Unit_2_C_Programming/06-Macros/03-Debug_2.c
@@ -4,28 +4,28 @@
 #define DEBUG 1
 void debug_variable(const char *fmt, ...)
 {
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	va_end(args);
 }
 
 /* debug_var(("var1 = %d", var1)); */
-#define debug_var(x)                                                 \
-    do                                                               \
-    {                                                                \
-        if (DEBUG)                                                   \
-        {                                                            \
-            debug_variable("@ File: %s, Func: %s(), Line: %d ---> ", \
-                           __FILE__, __func__, __LINE__);            \
-            debug_variable x;                                        \
-        }                                                            \
-    } while (0)
+#define debug_var(x)                                                   \
+	do                                                                 \
+	{                                                                  \
+		if (DEBUG)                                                     \
+		{                                                              \
+			debug_variable("[File: %s , Line: %d , Func: %s()] ---> ", \
+						   __FILE__, __LINE__, __func__);              \
+			debug_variable x;                                          \
+		}                                                              \
+	} while (0)
 
 int main()
 {
-    int var1 = 5;
+	int var1 = 5;
 
-    debug_var(("var1 = %d , var1 = %d, var1 = %d, var1 = %d", var1++,var1++,++var1,var1));
-    return 0;
+	debug_var(("var1 = %d , var1 = %d, var1 = %d, var1 = %d", var1++, var1++, ++var1, var1));
+	return 0;
 }
